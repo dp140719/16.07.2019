275645b711bfce9a72df441935f1e60147c6ce2c
dp140719 <52877500+dp140719@users.noreply.github.com>
2019-07-16 10:40:31 +0300

    Add files via upload

---
+++ Adapter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            D3Shape d3Shape = new D3Shape();
+
+            draw(new Adapter3Dto2D(), d3Shape);
+        }
+
+        // non mutable
+        private static void draw(ID3Painter painter, D3Shape shape)
+        {
+            painter.Paint3DShape(shape);
+        }
+    }
+}
