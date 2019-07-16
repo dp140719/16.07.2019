833c78180693c2ee1252df6e8d0e097f18768bff
dp140719 <52877500+dp140719@users.noreply.github.com>
2019-07-16 13:41:02 +0300

    Add files via upload

---
+++ Memento/Memento/Context.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memento
+{
+    public class Context
+    {
+
+        TextCareTaker careTake = new TextCareTaker();
+
+        public void Save(RichTextBox richTex)
+        {
+            careTake.AddMemento(new TextMemento(richTex.Text));
+        }
+
+        public void Load(RichTextBox richText)
+        {
+            if (careTake.GetLast() != null && careTake.GetLast().savedText != null)
+                richText.Text = careTake.GetLast().savedText;
+        }
+    }
+}
---
+++ Memento/Memento/TextCareTaker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    class TextCareTaker
+    {
+        TextMemento memento = null;
+
+        public void AddMemento(TextMemento memento)
+        {
+            this.memento = memento;
+        }
+
+        public TextMemento GetLast()
+        {
+            return this.memento;
+        }
+    }
+}
